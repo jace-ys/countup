0c92c52a8cfbe9b049d890c1c1b8913414559bb2
jace-ys <jace@duffel.com>
2024-10-21 00:15:25 +0100

    Implement

---
+++ app/internal/endpoints/goa.go
@@ -0,0 +1,45 @@
+package endpoints
+
+import (
+	"goa.design/clue/debug"
+	"goa.design/clue/log"
+	goa "goa.design/goa/v3/pkg"
+
+	"github.com/jace-ys/countup/internal/endpoints/middleware/goaerror"
+	"github.com/jace-ys/countup/internal/endpoints/middleware/tracer"
+)
+
+type GoaAdapter[S any, E GoaEndpoints] struct {
+	newFunc GoaNewFunc[S, E]
+}
+
+type GoaEndpoints interface {
+	Use(func(goa.Endpoint) goa.Endpoint)
+}
+
+type GoaNewFunc[S any, E GoaEndpoints] func(svc S) E
+
+func Goa[S any, E GoaEndpoints](newFunc GoaNewFunc[S, E]) *GoaAdapter[S, E] {
+	return &GoaAdapter[S, E]{
+		newFunc: newFunc,
+	}
+}
+
+func (a *GoaAdapter[S, E]) Adapt(svc S) E {
+	ep := a.newFunc(svc)
+
+	chainMiddleware(ep,
+		tracer.Endpoint,
+		log.Endpoint,
+		debug.LogPayloads(),
+		goaerror.Reporter,
+	)
+
+	return ep
+}
+
+func chainMiddleware[E GoaEndpoints](ep E, m ...func(goa.Endpoint) goa.Endpoint) {
+	for i := len(m) - 1; i >= 0; i-- {
+		ep.Use(m[i])
+	}
+}
